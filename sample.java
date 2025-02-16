4557b114accf6f949315d2d0558980b1a65f2e8c
KwameAmoahh <kwame.amoah498@gmail.com>
2025-02-16 16:20:13 +0200

    Updated Code

--- sample.java
+++ sample.java
@@ -1,6 +1,8 @@
 class sample{
 
     public static void main (String[] args){
-        System.out.print("Yow");
+        System.out.println("Yow");
+
+        System.out.println("Changes were made!!!");
     }
 }
